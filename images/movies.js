0913747c0159fd81871ac8328e7e6ffdb7dde245
Veronica Velikaya <nik.velikay@gmail.com>
2021-05-20 15:33:41 +0300

    uploaded files

---
+++ images/movies.js
@@ -0,0 +1,23 @@
+export const MOVIES =
+    [
+        {
+            id: 1,
+            title: 'Трафик',
+            image: 'images/img-1.png'
+        },
+        {
+            id: 2,
+            title: 'Рая та Дракон',
+            image: 'images/img-2.png'
+        },
+        {
+            id: 3,
+            title: 'Код: Железная Кора',
+            image: 'images/img-3.png'
+        },
+        {
+            id: 4,
+            title: 'Скайлайн 3',
+            image: 'images/img-4.png' 
+        }
+    ]
